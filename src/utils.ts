544b781004fdce2074c1e54b8c8a9026a0fc5b6f
hunghg255 <giahunghust@gmail.com>
2024-01-02 18:34:13 +0700

    chore: update

--- src/utils.ts
+++ src/utils.ts
@@ -38,20 +38,10 @@ export function svgHasOnlyPathChild(svg: string): boolean {
 
   if (mode) return true;
 
-  let path = 0;
   const fill = [];
 
   for (let idx = 0; idx < svgAst.children?.[0]?.children?.length; idx++) {
     const child = svgAst.children?.[0]?.children[idx];
-    const isRect = child?.name === 'rect' && child?.attributes?.fill;
-    const isCircle = child?.name === 'circle' && child?.attributes?.fill;
-    const isEllipse = child?.name === 'ellipse' && child?.attributes?.fill;
-    const isPolygon = child?.name === 'polygon' && child?.attributes?.fill;
-    const isPolyline = child?.name === 'polyline' && child?.attributes?.fill;
-
-    if (child.name === 'path' || isRect || isCircle || isEllipse || isPolygon || isPolyline) {
-      path = path + 1;
-    }
 
     if (child?.attributes?.fill && child?.attributes?.fill !== 'none') {
       fill.push(JSON.stringify(stringToColor(child?.attributes?.fill)));
@@ -61,16 +51,6 @@ export function svgHasOnlyPathChild(svg: string): boolean {
       for (let j = 0; j < child.children?.length; j++) {
         const child2 = child.children[j];
 
-        const isRect = child2?.name === 'rect' && child2?.attributes?.fill;
-        const isCircle = child2?.name === 'circle' && child2?.attributes?.fill;
-        const isEllipse = child2?.name === 'ellipse' && child2?.attributes?.fill;
-        const isPolygon = child2?.name === 'polygon' && child2?.attributes?.fill;
-        const isPolyline = child2?.name === 'polyline' && child2?.attributes?.fill;
-
-        if (child2.name === 'path' || isRect || isCircle || isEllipse || isPolygon || isPolyline) {
-          path = path + 1;
-        }
-
         if (child2?.attributes?.fill && child2?.attributes?.fill !== 'none') {
           fill.push(JSON.stringify(stringToColor(child2?.attributes?.fill)));
         }
@@ -78,16 +58,13 @@ export function svgHasOnlyPathChild(svg: string): boolean {
     }
   }
 
-  if (fill.length === 1) return true;
+  if (!fill.length || fill.length === 1) return true;
 
-  if (fill.length > 1) {
-    const fillSet = new Set(fill);
+  const fillSet = new Set(fill);
 
-    if (fillSet.size > 1) {
-      return false;
-    }
-    return true;
+  if (fillSet.size > 1) {
+    return false;
   }
 
-  return path > 1 ? false : true;
+  return true;
 }
