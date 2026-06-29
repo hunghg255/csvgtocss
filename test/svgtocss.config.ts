6193af5914caacf2abd84f27efafed9629f8aaf7
hunghg255 <giahunghust@gmail.com>
2026-06-29 10:39:49 +0700

    chore: update

--- test/svgtocss.config.ts
+++ test/svgtocss.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from '../dist/index.mjs';
 
 export default defineConfig({
   src: 'svg', // svg path
-  dist: 'dist/iconcss', // output path
+  dist: 'dist', // output path
   prefix: 'icon', // font name
   exportJson: true, // export json file
 });
